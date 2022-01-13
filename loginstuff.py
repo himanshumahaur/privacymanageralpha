a37866fcb478d30066a78a75801aa5afd4cd42ea
Himanshu Mahaur <76582074+himanshumahaur@users.noreply.github.com>
2022-01-13 10:43:51 +0530

    SHA256 to save passwords
    Creating users is now working.

--- loginstuff.py
+++ loginstuff.py
@@ -1,1 +1,31 @@
+from collections import UserList
+import hashlib
+import pandas as pd
+
+def getuserlist():
+    maindata = pd.read_csv('D:/.GitHub/privacymanageralpha/loginstuff.csv')
+    userlist = maindata['u'].values.tolist()
+    return userlist
+
+
+def userexists(username):
+    userlist = getuserlist()
+
+    if username in userlist:
+        return True
+    else:
+        return False
+
+
+def savenew():
+
+    username = str(input("Enter Username: "))
+    password = str(input("Enter Password: "))
+  
+    encripass = hashlib.sha256(password.encode())
+
+    newdata = pd.DataFrame({'user':[username], 'pass':[encripass.hexdigest()]})
+    newdata.to_csv('D:/.GitHub/privacymanageralpha/loginstuff.csv', mode='a', index=False, header=False)
+
+savenew()
 
